493c0a3f18d989abbbec38fec014f07c5a284859
IhTiYaR0 <ytpolyus@gmail.com>
2025-01-23 03:58:51 +0500

    Update Fish 5

--- fish/config.fish
+++ fish/config.fish
@@ -115,8 +115,52 @@ if status is-interactive
 
 
 
+set -U fish_color_user '#4379FF' 
+set -U fish_color_cwd '#4379FF' 
+set -U fish_color_command '#4379FF' 
 
-set -x XDG_SESSION_TYPE wayland
-set -x WAYLAND_DISPLAY wayland-0
+
+set -U fish_color_param green
+set -U fish_color_normal white
+set -U fish_color_selection white --background='#50FA7B'
+set -U fish_color_error '#FF5555'
+set -U fish_color_host '#FFFFFF'
+# set -U fish_color_autosuggestion brblack
+
+
+# Основной стиль текста
+# Основной стиль текста
+# set -U fish_color_normal '#50FA7B'                    # Обычный текст (неоново-зеленый)
+# set -U fish_color_command '#4379FF' --bold            # Команды (оранжевый, жирный)
+# set -U fish_color_param '#8BE9FD'                     # Аргументы команд (нежно-голубой)
+# set -U fish_color_cwd '#4379FF' --bold                # Текущий каталог (неоново-зеленый)
+# set -U fish_color_cwd_root '#FF5555' --bold           # Каталог root (красный, жирный)
+
+# # Приветствие в терминале (ihti@archlinux ~>)
+# set -U fish_color_user '#4379FF' --bold               # Имя пользователя (неоново-зеленый, жирный)
+# set -U fish_color_host '#50FA7B'                      # Хост (неоново-зеленый)
+# set -U fish_color_cwd '#4379FF' --bold                # Каталог (неоново-зеленый)
+
+# # Специальные элементы
+# set -U fish_color_comment '#6272A4'                   # Комментарии (спокойный синий)
+# set -U fish_color_error '#FF5555' --bold              # Ошибки (яркий красный)
+# set -U fish_color_match '#50FA7B' --bold              # Подсветка совпадений (неоново-зеленый)
+# set -U fish_color_selection black --background='#50FA7B'  # Выделение текста (черный на зелёном)
+# set -U fish_color_quote '#F1FA8C'                     # Кавычки (пастельный желтый)
+# set -U fish_color_redirection '#BD93F9'               # Перенаправления (фиолетовый)
+# set -U fish_color_operator '#FBAE23' --bold           # Операторы (`&&`, `||`)
+# set -U fish_color_escape '#FF79C6'                    # Экранированные символы (розовый)
+
+# # Автодополнения и пути
+# set -U fish_color_autosuggestion '#44475A'            # Автодополнения (темно-серый)
+# set -U fish_color_valid_path '#8BE9FD' --underline    # Валидные пути (голубой с подчеркиванием)
+
+# # Цвета для вывода файлов и папок (например, через ls)
+# set -Ux LS_COLORS 'di=1;38;5;208:ln=1;36:so=1;35:pi=1;33:bd=1;33;44:cd=1;33;44:or=1;31'
+
+
+
+#set -x XDG_SESSION_TYPE wayland
+#set -x WAYLAND_DISPLAY wayland-0
 
 end
